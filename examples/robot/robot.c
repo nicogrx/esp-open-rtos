793765de83be1138c34eec89c00256f9b1922d1a
Jean-Nicolas Graux <nicogrx@gmail.com>
2018-01-02 21:54:40 +0100

    examples: robot: add main task mngt
    Signed-off-by: Jean-Nicolas Graux <nicogrx@gmail.com>

--- examples/robot/robot.c
+++ examples/robot/robot.c
@@ -14,15 +14,29 @@
 #include <stdint.h>
 
 #include "leds.h"
+#include "pir.h"
 #include "utils.h"
 #include "http_server.h"
+
+static bool robot_task_end = false;
+
+static void robot_task(void *pvParameters) {
+	while(!robot_task_end) {
+		pir_wait_for_event();
+		leds_turn_on(BLUE);
+		delay_ms(1000);
+		leds_turn_off();
+	}
+	vTaskDelete(NULL);
+}
 
 void user_init(void)
 {
 	uart_set_baud(0, 115200);
-
-	http_server_init();
 	leds_init(24, 2);
+	pir_init(12);
+	http_server_init();
+	xTaskCreate(&robot_task, "robot mngt", 256, NULL, 2, NULL);
 }
 
 
