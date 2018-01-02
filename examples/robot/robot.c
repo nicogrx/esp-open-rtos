961e583c65af248cee60c141893494cf07d55697
Jean-Nicolas Graux <nicogrx@gmail.com>
2018-01-02 22:52:58 +0100

    examples: robot: use a timer to end on pir activity
    Signed-off-by: Jean-Nicolas Graux <nicogrx@gmail.com>

--- examples/robot/robot.c
+++ examples/robot/robot.c
@@ -9,6 +9,7 @@
 #include "FreeRTOS.h"
 #include "queue.h"
 #include "task.h"
+#include "timers.h"
 #include "esp/uart.h"
 #include <stdio.h>
 #include <stdint.h>
@@ -19,19 +20,33 @@
 #include "http_server.h"
 
 static bool robot_task_end = false;
+static TimerHandle_t on_pir_timer;
+
+static void pir_timer_cb(TimerHandle_t xTimer)
+{
+	leds_turn_off();
+}
 
 static void robot_task(void *pvParameters) {
 	int pir_ev;
+
+	on_pir_timer = xTimerCreate("on pir timer", 1000/portTICK_PERIOD_MS,
+								pdFALSE, NULL, pir_timer_cb);
+    if (on_pir_timer == NULL)
+		goto end;
+
 	while(!robot_task_end) {
-		if(pir_wait_for_event(&pir_ev))
-		{
+		if(pir_wait_for_event(&pir_ev)) {
 			printf("%s: ev at %i\n", __func__, pir_ev);
-			leds_turn_on(BLUE);
-			delay_ms(1000);
-			leds_turn_off();
+			if (xTimerStart(on_pir_timer, 0) != pdPASS) {
+				printf("%s: failed to start timer\n", __func__);
+			} else {
+				leds_turn_on(BLUE);
+			}
 		}
 		taskYIELD();
 	}
+end:
 	vTaskDelete(NULL);
 }
 
