e243025e916cc992439dbe1f7db6b249e2b82d8a
Jean-Nicolas Graux <nicogrx@gmail.com>
2018-01-02 21:53:46 +0100

    examples: robot: add pir driver
    Signed-off-by: Jean-Nicolas Graux <nicogrx@gmail.com>

---
+++ examples/robot/pir.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "semphr.h"
+#include "task.h"
+#include "timers.h"
+
+static bool pir_end = false;
+static SemaphoreHandle_t pir_sem;
+static QueueHandle_t pir_queue;
+
+static void pir_intr_handler(uint8_t gpio_num)
+{
+	static BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+	xSemaphoreGiveFromISR(pir_sem, &xHigherPriorityTaskWoken);
+}
+
+static void pir_task(void *pvParameters) {
+	QueueHandle_t queue = (QueueHandle_t)pvParameters;
+	int ticks;
+
+	while (!pir_end) {
+        xSemaphoreTake(pir_sem, portMAX_DELAY);
+		printf("%s: presence detected!\n", __func__);
+		ticks = xTaskGetTickCount();
+		xQueueSend(queue, &ticks, 0);
+	};
+	vTaskDelete(NULL);
+}
+
+int pir_wait_for_event(void)
+{
+	int ev;
+	xQueueReceive(pir_queue, &ev, portMAX_DELAY);
+	printf("%s: ev at %i\n", __func__, ev);
+	return ev;
+}
+
+int pir_init(uint8_t pin)
+{
+    pir_sem = xSemaphoreCreateBinary();
+	if (!pir_sem)
+		return -1;
+	pir_queue = xQueueCreate(2, sizeof(int));
+	if (!pir_queue)
+		return -1;
+    xTaskCreate(&pir_task, "pir mngt", 256, pir_queue, 2, NULL);
+    gpio_enable(pin, GPIO_INPUT);
+    gpio_set_interrupt(pin, GPIO_INTTYPE_EDGE_POS, pir_intr_handler);
+	return 0;
+}
---
+++ examples/robot/pir.h
@@ -0,0 +1,7 @@
+#ifndef PIR_H
+#define PIR_H
+
+int pir_init(uint8_t);
+int pir_wait_for_event(void);
+
+#endif
