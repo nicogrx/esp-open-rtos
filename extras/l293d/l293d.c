e01bf6fb4051fc97c04619e26d9e0dafedebc2e3
Jean-Nicolas Graux <nicogrx@gmail.com>
2018-01-13 22:39:43 +0100

    extras: l293d: support the gpio expander mode
    Signed-off-by: Jean-Nicolas Graux <nicogrx@gmail.com>

--- extras/l293d/l293d.c
+++ extras/l293d/l293d.c
@@ -8,36 +8,68 @@
 #include <esp/gpio.h>
 #include <espressif/esp_common.h>
 #include <stdio.h>
-#include "l293d.h"
+#include "i2c/i2c.h"
 #include "pwm/pwm.h"
+#include "l293d.h"
 
-void l293d_init(const struct l293d_device *dev)
+int l293d_init(struct l293d_device *dev)
 {
     uint8_t enable_pins[2];
+	if (!dev)
+		return -1;
+	if (dev->init_done)
+		return -1;
 
-    gpio_enable(dev->input_a1_pin, GPIO_OUTPUT);
-    gpio_enable(dev->input_a2_pin, GPIO_OUTPUT);
-    gpio_enable(dev->input_b1_pin, GPIO_OUTPUT);
-    gpio_enable(dev->input_b2_pin, GPIO_OUTPUT);
-    gpio_write(dev->input_a1_pin, false);
-    gpio_write(dev->input_a2_pin, false);
-    gpio_write(dev->input_b1_pin, false);
-    gpio_write(dev->input_b2_pin, false);
-
-	enable_pins[0] = dev->enable_1_pin;
-	enable_pins[1] = dev->enable_2_pin;
-    pwm_init(2, enable_pins, false);
-    pwm_set_freq(1000);
-	pwm_stop();
+	switch (dev->mode) {
+	case GPIO_ESP:
+		gpio_enable(dev->input_a1_pin, GPIO_OUTPUT);
+		gpio_enable(dev->input_a2_pin, GPIO_OUTPUT);
+		gpio_enable(dev->input_b1_pin, GPIO_OUTPUT);
+		gpio_enable(dev->input_b2_pin, GPIO_OUTPUT);
+		gpio_write(dev->input_a1_pin, false);
+		gpio_write(dev->input_a2_pin, false);
+		gpio_write(dev->input_b1_pin, false);
+		gpio_write(dev->input_b2_pin, false);
+		enable_pins[0] = dev->enable_1_pin;
+		enable_pins[1] = dev->enable_2_pin;
+		pwm_init(2, enable_pins, false);
+		pwm_set_freq(1000);
+		pwm_stop();
+		break;
+	case GPIO_EXPANDER:
+		if (!dev->i2c)
+			return -1;
+		if (!dev->gpio_write)
+			return -1;
+		dev->gpio_write(dev->i2c, dev->input_a1_pin, false);
+		dev->gpio_write(dev->i2c, dev->input_a2_pin, false);
+		dev->gpio_write(dev->i2c, dev->input_b1_pin, false);
+		dev->gpio_write(dev->i2c, dev->input_b2_pin, false);
+		dev->gpio_write(dev->i2c, dev->enable_1_pin, false);
+		dev->gpio_write(dev->i2c, dev->enable_2_pin, false);
+		break;
+	default:
+		return -1;
+	}
+	dev->init_done = true;
+	return 0;
 }
 
 
 int l293d_dc_motors_stop(const struct l293d_device *dev)
 {
-	if (!dev)
+	if (!dev || !dev->init_done)
 		return -1;
-
-	pwm_stop();
+	
+	switch (dev->mode) {
+	case GPIO_ESP:
+		pwm_stop();
+		break;
+	case GPIO_EXPANDER:
+		dev->gpio_write(dev->i2c, dev->enable_1_pin, false);
+		dev->gpio_write(dev->i2c, dev->enable_2_pin, false);
+		break;
+	}
 	return 0;
 	
 }
@@ -45,12 +77,21 @@ int l293d_dc_motors_stop(const struct l293d_device *dev)
 int l293d_dc_motors_start(const struct l293d_device *dev, uint8_t speed)
 {
 	static bool first_start = true;
-	if (!dev)
+	if (!dev || !dev->init_done)
 		return -1;
-    pwm_set_duty(UINT16_MAX * speed / 255);
-	if (first_start) {
-		first_start = false;
-		pwm_start();
+
+	switch (dev->mode) {
+	case GPIO_ESP:
+    	pwm_set_duty(UINT16_MAX * speed / 255);
+		if (first_start) {
+			first_start = false;
+			pwm_start();
+		}
+		break;
+	case GPIO_EXPANDER:
+		dev->gpio_write(dev->i2c, dev->enable_1_pin, true);
+		dev->gpio_write(dev->i2c, dev->enable_2_pin, true);
+		break;
 	}
 	return 0;
 }
@@ -59,9 +100,9 @@ int l293d_dc_motor_rotate(const struct l293d_device *dev, int motor, int dir)
 {
 	uint8_t input1, input2;
 
-	if (!dev)
+	if (!dev || !dev->init_done)
 		return -1;
-
+	
 	switch (motor) {
 	case L293D_M1:
 		input1 = dev->input_a1_pin;
@@ -75,25 +116,51 @@ int l293d_dc_motor_rotate(const struct l293d_device *dev, int motor, int dir)
 		return -1;
 	}
 
-	switch (dir) {
-	case L293D_CLOCKWISE:
-		gpio_write(input1, true);
-		gpio_write(input2, false);
+	switch (dev->mode) {
+	case GPIO_ESP:
+		switch (dir) {
+		case L293D_CLOCKWISE:
+			gpio_write(input1, true);
+			gpio_write(input2, false);
+			break;
+		case L293D_ANTI_CLOCKWISE:
+			gpio_write(input1, false);
+			gpio_write(input2, true);
+			break;
+		case L293D_IDLE_HI:
+			gpio_write(input1, false);
+			gpio_write(input2, false);
+			break;
+		case L293D_IDLE:
+			gpio_write(input1, true);
+			gpio_write(input2, true);
+			break;
+		default:
+			return -1;
+		}
 		break;
-	case L293D_ANTI_CLOCKWISE:
-		gpio_write(input1, false);
-		gpio_write(input2, true);
+	case GPIO_EXPANDER:
+		switch (dir) {
+		case L293D_CLOCKWISE:
+			dev->gpio_write(dev->i2c, input1, true);
+			dev->gpio_write(dev->i2c, input2, false);
+			break;
+		case L293D_ANTI_CLOCKWISE:
+			dev->gpio_write(dev->i2c, input1, false);
+			dev->gpio_write(dev->i2c, input2, true);
+			break;
+		case L293D_IDLE_HI:
+			dev->gpio_write(dev->i2c, input1, false);
+			dev->gpio_write(dev->i2c, input2, false);
+			break;
+		case L293D_IDLE:
+			dev->gpio_write(dev->i2c, input1, true);
+			dev->gpio_write(dev->i2c, input2, true);
+			break;
+		default:
+			return -1;
+		}
 		break;
-	case L293D_IDLE_HI:
-		gpio_write(input1, false);
-		gpio_write(input2, false);
-		break;
-	case L293D_IDLE:
-		gpio_write(input1, true);
-		gpio_write(input2, true);
-		break;
-	default:
-		return -1;
 	}
 	return 0;
 }
--- extras/l293d/l293d.h
+++ extras/l293d/l293d.h
@@ -20,20 +20,31 @@ enum dc_motor_direction {
 	L293D_ANTI_CLOCKWISE
 };
 
+enum gpio_mode {
+	GPIO_ESP,
+	GPIO_EXPANDER,
+};
+
 /*
  * l293d device struct
  */
 struct l293d_device
 {
+	bool init_done;
     uint8_t enable_1_pin;
     uint8_t input_a1_pin;
     uint8_t input_a2_pin;
     uint8_t enable_2_pin;
     uint8_t input_b1_pin;
     uint8_t input_b2_pin;
+	uint8_t mode;
+	i2c_dev_t *i2c;
+	void (* gpio_write)(i2c_dev_t *dev,
+						uint8_t num,
+						bool value);
 };
 
-void l293d_init(const struct l293d_device *dev);
+int l293d_init(struct l293d_device *dev);
 int l293d_dc_motors_start(const struct l293d_device *dev, uint8_t speed);
 int l293d_dc_motors_stop(const struct l293d_device *dev);
 int l293d_dc_motor_rotate(const struct l293d_device *dev, int motor, int dir);
